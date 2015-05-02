fee2d6adbdb6898743f48b9d7ce4f3c7a0964b6a
geraint <palmer.geraint@googlemail.com>
2015-05-02 13:17:45 +0100

    Title page done using render

--- sklDj/frontend/views.py
+++ sklDj/frontend/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. This is the title page.")
+    return render(request, 'frontend/index.html')
