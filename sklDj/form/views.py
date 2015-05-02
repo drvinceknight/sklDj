cc227b54e5f773c9a069f24e2c1f6b2cea105b30
Vince Knight <vincent.knight@gmail.com>
2015-05-02 19:49:38 +0100

    Implementing mpld3 in the views
    In the form view there is a plot function that returns an html object of
    the d3 plot.
    This is currently looking at the online versions of mpld3 javascript
    libraries which when we have time will need to be changed.

--- sklDj/form/views.py
+++ sklDj/form/views.py
@@ -4,10 +4,18 @@ from django.template import RequestContext
 from .forms import UploadFileForm
 
 import csv
+import matplotlib.pyplot as plt, mpld3
+import json
 # Imaginary function to handle an uploaded file.
-def handle_uploaded_file(f):
-    data = [row for row in csv.reader(f.read().splitlines())]
-    return data
+def plot(f):
+    data = [[eval(k) for k in row] for row in csv.reader(f.read().splitlines())]
+    x = [row[0] for row in data]
+    y = [row[1] for row in data]
+
+    fig = plt.figure()
+    plt.scatter(x, y)
+    fig_html = mpld3.fig_to_html(fig) # When we have local mpld3 libraries we will need to tweak this
+    return fig_html
 
 
 def upload_file(request):
@@ -15,8 +23,8 @@ def upload_file(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             name = request.FILES['file']
-            data = handle_uploaded_file(name)
-            return render_to_response('form/success.html', {'data': data, 'name': name})
+            fig_html = plot(name)
+            return render_to_response('form/success.html', {"plot": fig_html, 'name': name})
     else:
         form = UploadFileForm()
     return render_to_response('form/upload.html', {'form': form},
