c397ec0f26110c29400178fcfe112f5f2a9d800f
Vince Knight <vincent.knight@gmail.com>
2015-05-03 15:32:12 +0100

    Somehow it works
    - The implementations library is being read.
    - Everything is glorious.

--- sklDj/frontend/views.py
+++ sklDj/frontend/views.py
@@ -5,8 +5,10 @@ from frontend.models import Machine_Learning_Models
 from .forms import UploadFileForm
 
 import csv
-import matplotlib.pyplot as plt, mpld3
-from sklearn.linear_model import LinearRegression
+#import matplotlib.pyplot as plt, mpld3
+#from sklearn.linear_model import LinearRegression
+
+from implementations import LinearRegression
 
 def index(request):
     algorithms_list = Machine_Learning_Models.objects.all()
@@ -30,38 +32,18 @@ def model_upload(request, model_name):
     form = UploadFileForm(request.POST, request.FILES)
     if form.is_valid():
         name = request.FILES['file']
-        fig_html = plot(name)
+        data = read_data(name)
+        implementation = LinearRegression(data)
+        fig_html, coeff, intercept = implementation.run()
         return render_to_response('frontend/results.html', {'fig_html': fig_html,
-            'algorithm': algorithm, 'name':name})
+            'algorithm': algorithm, 'name':name, 'coeff': coeff})
     #else:
         #form = UploadFileForm()
     #context = {'algorithms_list': algorithms_list, 'form':form}
     #return render_to_response('frontend/index.html', context,
             #context_instance=RequestContext(request))
 
-# Imaginary function to handle an uploaded file.
-def handle_uploaded_file(f):
-    data = [row for row in csv.reader(f.read().splitlines())]
-    return data
-
-
 ##############
-
-
-# Imaginary function to handle an uploaded file.
-def plot(f):
+def read_data(f):
     data = [[eval(k) for k in row] for row in csv.reader(f.read().splitlines())]
-    x = [[row[0]] for row in data]
-    y = [row[1] for row in data]
-
-    algorithm = LinearRegression()
-    algorithm.fit(x,y)
-
-
-    m = algorithm.coef_
-    b = algorithm.intercept_
-    fig = plt.figure()
-    plt.scatter(x, y)
-    plt.plot([min(x)[0],max(x)[0]], [m*min(x)[0] + b, m*max(x)[0] + b])
-    fig_html = mpld3.fig_to_html(fig) # When we have local mpld3 libraries we will need to tweak this
-    return fig_html
+    return data
