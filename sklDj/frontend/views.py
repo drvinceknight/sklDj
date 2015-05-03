2606215db97e2b53ea464db0ff303d33e0748583
Jason Young <jas.young314@gmail.com>
2015-05-03 11:50:53 +0100

    Commiting things before Vince works on them
    - Have copied template in forms to frontend
    - Changed view
    - Modifying css.

--- sklDj/frontend/views.py
+++ sklDj/frontend/views.py
@@ -8,8 +8,17 @@ import csv
 
 def index(request):
     algorithms_list = Machine_Learning_Models.objects.all()
-    context = {'algorithms_list': algorithms_list}
-    return render(request, 'frontend/index.html', context)
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            name = request.FILES['file']
+            data = handle_uploaded_file(name)
+            return render_to_response('form/success.html', {'data': data, 'name': name})
+    else:
+        form = UploadFileForm()
+    context = {'algorithms_list': algorithms_list, 'form':form}
+    return render_to_response('frontend/index.html', context,
+            context_instance=RequestContext(request))
 
 def model_info(request, model_name):
     algorithm = get_object_or_404(Machine_Learning_Models, slug=model_name)
@@ -22,15 +31,3 @@ def handle_uploaded_file(f):
     data = [row for row in csv.reader(f.read().splitlines())]
     return data
 
-
-def upload_file(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            name = request.FILES['file']
-            data = handle_uploaded_file(name)
-            return render_to_response('form/success.html', {'data': data, 'name': name})
-    else:
-        form = UploadFileForm()
-    return render_to_response('form/upload.html', {'form': form},
-            context_instance=RequestContext(request))
