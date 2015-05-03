d645d327c482d3acdcec546ecbf125ab72d95ce3
Jason Young <jas.young314@gmail.com>
2015-05-03 17:29:08 +0100

    Put example code on index
    - Added example code to the models
    - Added code to the index template to pull the example code out

--- sklDj/frontend/models.py
+++ sklDj/frontend/models.py
@@ -6,6 +6,7 @@ class Machine_Learning_Models(models.Model):
     name = models.CharField(blank=True, max_length=200)
     description = models.TextField(blank=True)
     link = models.URLField(blank=True)
+    example_code = models.TextField(blank=True)
     slug = models.CharField(max_length=200)
 
     def __str__(self):
