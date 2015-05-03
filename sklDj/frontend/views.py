70a9561c5762d93e594a1af468714b8fe95a42e0
Vince Knight <vincent.knight@gmail.com>
2015-05-03 14:43:12 +0100

    File uploaders now work for each algorithm work.

--- sklDj/frontend/views.py
+++ sklDj/frontend/views.py
@@ -5,16 +5,11 @@ from frontend.models import Machine_Learning_Models
 from .forms import UploadFileForm
 
 import csv
+import matplotlib.pyplot as plt, mpld3
+from sklearn.linear_model import LinearRegression
 
 def index(request):
     algorithms_list = Machine_Learning_Models.objects.all()
-    #if request.method == 'POST':
-        #form = UploadFileForm(request.POST, request.FILES)
-        #if form.is_valid():
-            #name = request.FILES['file']
-            #data = handle_uploaded_file(name)
-            #return render_to_response('form/success.html', {'data': data, 'name': name})
-    #else:
     form = UploadFileForm()
     context = {'algorithms_list': algorithms_list, 'form': form}
     return render_to_response('frontend/index.html', context,
@@ -29,25 +24,44 @@ def model_upload(request, model_name):
     algorithms_list = Machine_Learning_Models.objects.all()
     algorithm = get_object_or_404(Machine_Learning_Models, slug=model_name)
     # Assuming request.method is always POST
-    form = UploadFileForm(request.POST, request.FILES)
+    #form = UploadFileForm(request.POST, request.FILES)
 
-    print 'woof'
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            name = request.FILES['file']
-            data = handle_uploaded_file(name)
-            return render_to_response('frontend/results.html', {'data': data,
-                'algorithm': algorithm, 'name':name})
-    else:
-        form = UploadFileForm()
-    context = {'algorithms_list': algorithms_list, 'form':form}
-    return render_to_response('frontend/index.html', context,
-            context_instance=RequestContext(request))
-    #return render(request, 'frontend/results.html', context)
+    #if request.method == 'POST':
+    form = UploadFileForm(request.POST, request.FILES)
+    if form.is_valid():
+        name = request.FILES['file']
+        fig_html = plot(name)
+        return render_to_response('frontend/results.html', {'fig_html': fig_html,
+            'algorithm': algorithm, 'name':name})
+    #else:
+        #form = UploadFileForm()
+    #context = {'algorithms_list': algorithms_list, 'form':form}
+    #return render_to_response('frontend/index.html', context,
+            #context_instance=RequestContext(request))
 
 # Imaginary function to handle an uploaded file.
 def handle_uploaded_file(f):
     data = [row for row in csv.reader(f.read().splitlines())]
     return data
 
+
+##############
+
+
+# Imaginary function to handle an uploaded file.
+def plot(f):
+    data = [[eval(k) for k in row] for row in csv.reader(f.read().splitlines())]
+    x = [[row[0]] for row in data]
+    y = [row[1] for row in data]
+
+    algorithm = LinearRegression()
+    algorithm.fit(x,y)
+
+
+    m = algorithm.coef_
+    b = algorithm.intercept_
+    fig = plt.figure()
+    plt.scatter(x, y)
+    plt.plot([min(x)[0],max(x)[0]], [m*min(x)[0] + b, m*max(x)[0] + b])
+    fig_html = mpld3.fig_to_html(fig) # When we have local mpld3 libraries we will need to tweak this
+    return fig_html
