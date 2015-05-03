50ebef0812bbfc55f998706b7286f0ec73c582db
Vince Knight <vincent.knight@gmail.com>
2015-05-03 14:11:06 +0100

    Have uploader working.

--- sklDj/frontend/views.py
+++ sklDj/frontend/views.py
@@ -8,15 +8,15 @@ import csv
 
 def index(request):
     algorithms_list = Machine_Learning_Models.objects.all()
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            name = request.FILES['file']
-            data = handle_uploaded_file(name)
-            return render_to_response('form/success.html', {'data': data, 'name': name})
-    else:
-        form = UploadFileForm()
-    context = {'algorithms_list': algorithms_list, 'form':form}
+    #if request.method == 'POST':
+        #form = UploadFileForm(request.POST, request.FILES)
+        #if form.is_valid():
+            #name = request.FILES['file']
+            #data = handle_uploaded_file(name)
+            #return render_to_response('form/success.html', {'data': data, 'name': name})
+    #else:
+    form = UploadFileForm()
+    context = {'algorithms_list': algorithms_list, 'form': form}
     return render_to_response('frontend/index.html', context,
             context_instance=RequestContext(request))
 
@@ -26,9 +26,25 @@ def model_info(request, model_name):
     return render(request, 'frontend/models_info.html', context)
 
 def model_upload(request, model_name):
+    algorithms_list = Machine_Learning_Models.objects.all()
     algorithm = get_object_or_404(Machine_Learning_Models, slug=model_name)
-    context = {'algorithm': algorithm}
-    return render(request, 'frontend/results.html', context)
+    # Assuming request.method is always POST
+    form = UploadFileForm(request.POST, request.FILES)
+
+    print 'woof'
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            name = request.FILES['file']
+            data = handle_uploaded_file(name)
+            return render_to_response('frontend/results.html', {'data': data,
+                'algorithm': algorithm, 'name':name})
+    else:
+        form = UploadFileForm()
+    context = {'algorithms_list': algorithms_list, 'form':form}
+    return render_to_response('frontend/index.html', context,
+            context_instance=RequestContext(request))
+    #return render(request, 'frontend/results.html', context)
 
 # Imaginary function to handle an uploaded file.
 def handle_uploaded_file(f):
